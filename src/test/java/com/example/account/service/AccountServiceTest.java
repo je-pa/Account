1f80a552521b7e293cde770689bcb59535663218
je-pa <jieun0502@gmail.com>
2024-01-20 01:45:29 +0900

    test: Mokito & Controller test(@WebMvcTest)

--- src/test/java/com/example/account/service/AccountServiceTest.java
+++ src/test/java/com/example/account/service/AccountServiceTest.java
@@ -2,23 +2,44 @@ package com.example.account.service;
 
 import com.example.account.domain.Account;
 import com.example.account.domain.AccountStatus;
-import org.junit.jupiter.api.BeforeEach;
+import com.example.account.repository.AccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
 
-@SpringBootTest//di
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)// 확장팩
 class AccountServiceTest {
-    @Autowired
+    @Mock
+    private AccountRepository accountRepository;
+
+    @InjectMocks
     private AccountService accountService;
 
-    @BeforeEach
-    void init() {
-        accountService.createAccount();
+    @Test
+    @DisplayName("계좌 조회 성공")
+    void testXXX(){
+        // given
+        given(accountRepository.findById(anyLong()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountStatus(AccountStatus.UNREGISTERED)
+                        .accountNumber("65789").build()));
+        // when
+        Account account = accountService.getAccount(4555L);
+
+        // then
+        assertEquals("65789", account.getAccountNumber());
+        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
     }
+
     @Test
     @DisplayName("Test 이름 변경")
     void testGetAccount(){
