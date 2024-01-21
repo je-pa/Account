3ed4a5fc571f92a431f6082c294dffb30ddcace8
je-pa <jieun0502@gmail.com>
2024-01-21 19:28:34 +0900

    valid

--- src/main/java/com/example/account/dto/CreateAccount.java
+++ src/main/java/com/example/account/dto/CreateAccount.java
@@ -1,5 +1,8 @@
 package com.example.account.dto;
 
+import com.esotericsoftware.kryo.serializers.FieldSerializer;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -8,7 +11,13 @@ public class CreateAccount {
     @Getter
     @Setter
     public static class Request{
+
+        @NotNull
+        @Min(1)
         private Long userId;
+
+        @NotNull
+        @Min(100)
         private Long initialBalance;
 
     }
